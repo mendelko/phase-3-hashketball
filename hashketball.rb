5603bc6c225fe6bdaac0174a7925269b8ac6ab27
Mendel <mkonikov123@gmail.com>
2021-08-28 01:22:14 -0400

    commit tree e25fd9ff7b99314ac30e6da3397014aa6155e3cc

--- hashketball.rb
+++ hashketball.rb
@@ -127,3 +127,69 @@ def game_hash
 end
 
 # Write code here
+
+def num_points_scored(name)
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player|
+      if player[:player_name] == name
+        return player[:points]
+      end
+    end
+  end
+end
+
+def shoe_size(name)
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player|
+      if player[:player_name] == name
+        return player[:shoe]
+      end
+    end
+  end
+end
+
+def team_colors(team_name)
+  game_hash.each do |location, team_data|
+    if team_data[:team_name] == team_name
+      return team_data[:colors]
+    end
+  end
+end
+
+def team_names
+  game_hash.map do |location, team_data|
+    team_data[:team_name]
+  end
+end
+
+def player_numbers(team_name)
+  arr = []
+  game_hash.each do |location, team_data|
+    if team_data[:team_name] == team_name
+      team_data[:players].each do |player|
+        arr.push(player[:number])
+      end
+    end
+  end
+  return arr
+end
+
+def player_stats(name)
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player|
+      if player[:player_name] == name
+        return player
+      end
+    end
+  end
+end
+
+def big_shoe_rebounds
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player|
+      if player[:shoe] == 19
+        return player[:rebounds]
+      end
+    end
+  end
+end
